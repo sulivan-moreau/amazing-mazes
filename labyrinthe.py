62cea09226ddcae69158956ab7ab523ebf5c165f
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-09 16:59:33 +0200

    Ajout du début et de la fin du labyrinthe

--- labyrinthe.py
+++ labyrinthe.py
@@ -3,16 +3,18 @@ passs= "."
 
 def create_list():
     nb_enter = int(input())
-    name_file = input()
+    name_file = input("Nom du fichier : ")
     
     matrice = [[wall]*nb_enter for _ in range(nb_enter)]
+    matrice[0][0] = passs
+    matrice[nb_enter-1][nb_enter-1] = passs
     
     for ligne in matrice:
         print(ligne,"\n")
-        
-    f = open("new_file.txt","w")
-    for ligne in matrice:
-        for i in ligne:
-            f.write(i)
-    
+
+    with open(name_file, 'w') as file:
+        for ligne in matrice:
+            for i in ligne:
+                file.write(''.join(i) + '\n')
+                
 create_list()
