f926ee88f793637d189cb1e33ef6ab42a096da75
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-11 13:03:27 +0200

    Ajout du backtracking avec couleur

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,108 +1,11 @@
-# import random
+import random
+import colorama
+from colorama import Fore, Style
 
-# wall = "#"
-# passs = "."
-# nb_enter = int(input("Number enter : "))
-
-# def create_matrice():
-#     nb = (nb_enter*2)+1
-#     string = wall*nb
-#     matrice = [string for _ in range(nb)]
-    
-#     for ligne in matrice:
-#         print(ligne, "\n")
-#     text = matrice[0]
-#     caractere = list(text)
-#     caractere[0][0] = "X"
-#     text = "".join(caractere)
-
-#     return matrice
-
-# def create_file():
-#     matrice = create_matrice()
-#     name_file = input("Nom du fichier : ")
-    
-#     # matrice[nb_enter - 1][nb_enter - 1] = passs
-
-#     with open(f"{name_file}.txt", "w") as file:
-#         for ligne in matrice:
-#             file.write("".join(ligne) + "\n")
-
-# # def modify_matrice():
-    
-    
-# create_file()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # for i, ligne in enumerate(matrice):
-    #     a = random.randint(0, nb_enter - 1)
-    #     matrice[i][a] = passs
-
-
-# random_matrice()
-
-# def random_matrice():
-#     matrice = [[wall] * nb_enter for _ in range(nb_enter)]
-#     matrice[0][0] = passs
-#     matrice[nb_enter - 1][nb_enter - 1] = passs
-
-#     for i, ligne in enumerate(matrice):
-#         a = random.randint(0, nb_enter - 1)
-#         matrice[i][a] = passs
-
-#     for ligne in matrice:
-#         print(ligne, "\n")
-
-
-
-
-# import random
-
-# wall = "#"
-# passs = "."
-# nb_enter = int(input("Number enter : "))
-
-# def create_matrice():
-#     nb = (nb_enter * 2) + 1
-#     string = [wall] * nb
-#     matrice = [string[:] for _ in range(nb)]
-
-#     for ligne in matrice:
-#         print("".join(ligne), "\n")
-
-#     return matrice
-
-# def create_file():
-#     matrice = create_matrice()
-#     name_file = input("Nom du fichier : ")
-
-#     with open(f"{name_file}.txt", "w") as file:
-#         for ligne in matrice:
-#             file.write("".join(ligne) + "\n")
-
-# create_file()
-
-
+colorama.init(autoreset=True)
 
 def generation_matrice():
     mur = "#"
-    chemin = "."
     entree = "E"
     sortie = "S"
 
@@ -110,32 +13,56 @@ def generation_matrice():
     taille_mat = (taille_mat * 2) + 1
 
     mat = [[mur for _ in range(taille_mat)] for _ in range(taille_mat)]
-    
-    
-    for i in range(taille_mat):
-        mat[0][i] = mur
-        mat[taille_mat - 1][i] = mur
 
     for i in range(taille_mat):
-        mat[i][0] = mur
-        mat[i][taille_mat - 1] = mur
+        mat[0][i] = mat[taille_mat - 1][i] = mat[i][0] = mat[i][taille_mat - 1] = mur
 
-    
     mat[1][1] = entree
     mat[taille_mat - 2][taille_mat - 2] = sortie
 
-   
-    for ligne in mat:
-        print(" ".join(ligne))
+    generer_chemin_backtracking(mat, 1, 1)
 
-    return mat 
+    afficher_matrice(mat)
 
-    
+    return mat
+
+def generer_chemin_backtracking(mat, x, y):
+    pile = [(x, y)]
+    while pile:
+        x, y = pile[-1]
+        voisins = obtenir_voisins(mat, x, y)
+        if not voisins:
+            pile.pop()
+        else:
+            nx, ny = random.choice(voisins)
+            mat[nx][ny] = "."
+            mat[(x + nx) // 2][(y + ny) // 2] = "."
+            pile.append((nx, ny))
+
+def obtenir_voisins(mat, x, y):
+    directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]  # droite, bas, gauche, haut
+    voisins = []
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(mat) and 0 <= ny < len(mat) and mat[nx][ny] == "#":
+            voisins.append((nx, ny))
+    return voisins
 
 def sauvegarde_fichier(mat):
     nom_fichier = input("Nom du fichier de sauvegarde : ")
     with open(nom_fichier + ".txt", "w") as fichier:
         for ligne in mat:
             fichier.write(" ".join(ligne) + "\n")
-            
-generation_matrice()
+
+def afficher_matrice(mat):
+    for ligne in mat:
+        ligne_affichee = ""
+        for cellule in ligne:
+            if cellule == ".":
+                ligne_affichee += Fore.BLUE + cellule + Style.RESET_ALL
+            else:
+                ligne_affichee += cellule
+        print(ligne_affichee)
+
+mat = generation_matrice()
+sauvegarde_fichier(mat)
