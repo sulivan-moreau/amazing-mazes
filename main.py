5da6d8f0adbde3d807267772915bc03db4c9eb38
NovaStarmax <lapvpgames@gmail.com>
2024-09-12 13:49:46 +0200

    Fix file generater

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-from colorama import Fore, Style
+# from colorama import Fore, Style
 CHAR_GROUND = "."
 CHAR_WALL = "#"
 
-def size_matrice():
+# def nouvelle():
+#     while True:
+#         try :
+#             n = int(input())
+
+
+def get_matrix_size():
     n = input("Map size : ")
-    if n.isnumeric()==True:
+    if n.isnumeric() == True:
         n = int(n)
-        valid = f"{Fore.GREEN}Valid map size{Style.RESET_ALL}"
-        print(valid)
+        # valid = f"{Fore.GREEN}Valid map size{Style.RESET_ALL}"
+        #  print(valid)
         return n
-    else: 
-        error = f"{Fore.RED}To enter the map size please enter a number{Style.RESET_ALL}"
-        print(error)
-        size_matrice()
+    else:
+        # error = f"{Fore.RED}To enter the map size please enter a number{Style.RESET_ALL}"
+        # print(error)
+        return get_matrix_size()
+
 
 def generate_maze():
-    n = size_matrice()
+    n = get_matrix_size()
     m = []
-    for _ in range((n *2)+1):
-        m.append([CHAR_WALL] * ((n *2)+1))
+    for _ in range(n * 2 + 1):
+        m.append([CHAR_WALL] * (n * 2 + 1))
 
     # entrée et sortie
-    # m[1][0] = CHAR_GROUND
+    m[1][0] = CHAR_GROUND
     m[1][1] = CHAR_GROUND
     m[-2][-2] = CHAR_GROUND
-    # m[-2][-1] = CHAR_GROUND
-
-    for ligne in m:
-        print(" ".join(ligne))
+    m[-2][-1] = CHAR_GROUND
 
     return m
 
-def sauvegarde_fichier(m):
+
+def save_file(m):
     file_name = input("Write the name of your map without spaces: ")
     with open(f"{file_name}.txt", "a") as file:
         for ligne in m:
             file.write("".join(ligne) + "\n")
-            
-created_maze = generate_maze()
-sauvegarde_fichier(created_maze)
 
+
+def print_maze(maze):
+    for line in maze:
+        print(" ".join(line))
+
+
+def main():
+    maze = generate_maze()
+    print_maze(maze)
+    save_file(maze)
+
+
+if __name__ == "__main__":
+    main()
