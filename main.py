349a3a6797bb71971416ab4c6c7464b89ad22532
ines-lorquet <ines.lorquet@laplateforme.io>
2024-09-13 11:36:08 +0200

    [Feature maze_rec]

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import random
 from colorama import Fore, Style
 CHAR_GROUND = "."
 CHAR_WALL = "#"
+DIRECTIONS = [(2,0),(-2,0),(0,-2),(0,2)]
 
 def get_matrix_size():
     while True:
         try:
-            n = int(input())
-            
+            n = int(input("Size : "))
+            print(type(n))
             return n
         except Exception:
             print(f"{Fore.RED}To enter the map size please enter a number{Style.RESET_ALL}")
         
 def generate_basic_matrix(n): 
     m = []
-    print(n,"sdfghujikolmù")
+    print(range (n*2),type(n),"Print de NNNN")
     for _ in range(n * 2 + 1):
         m.append([CHAR_WALL] * (n * 2 + 1))
 
@@ -31,23 +32,26 @@ def generate_maze(n):
     # La parti récursive doit pas être à chaque fois m 
     m = generate_basic_matrix(n)
     print(n,"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-    directions = [(2,0),(-2,0),(0,-2),(0,2)]
-
-    random.shuffle(directions)
     pos = [1,1]
-    for dx, dy in directions:
-        pos[0] += dx
-        pos[1] += dy
-        if m[pos[0]][pos[1]] == CHAR_WALL:
-            m[0][1] = CHAR_GROUND
-             
+    
+    def maze_rec(rank):
+        if pos == [n*2,n*2] or rank == 50:
+            return
+        random.shuffle(DIRECTIONS)
+        for dx, dy in DIRECTIONS:
+            pos[0] += dx
+            pos[1] += dy
+            if valid_neighbor(m,pos[0], pos[1]) == True:   
+                m[0][1] = CHAR_GROUND
+                maze_rec(rank+1)
+                
+    maze_rec(0)
     return m
 
 def valid_neighbor(m, x, y):
-    matrix_size = generate_maze(m)
     matrix_size = len(m)
     print(x,y,"XXXXXXXXXXXXXX YYYYYYYYYYYYYYYYYYYYYYY")
-    return 1 <= x < matrix_size - 1 and 1 <= y < matrix_size - 1 and m[x][y] == "#"
+    return 1 <= x < matrix_size - 1 and 1 <= y < matrix_size - 1 and m[x][y] == CHAR_WALL
 
 
 
@@ -65,7 +69,6 @@ def print_maze(maze):
 def main():
     n = get_matrix_size()
     maze = generate_maze(n)
-
     print_maze(maze)
     # save_file(maze)
 
